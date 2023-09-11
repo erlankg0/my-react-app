7de07111f8cbda7303bdc47413ae7237d7b59c5f
Erlan Abdraimov <62596605+erlankg0@users.noreply.github.com>
2023-09-11 14:07:06 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import logo from './logo.svg'
+import './App.css'
+import './style.css' // import style
+
+// Компонент - это функция или класс, который возвращающая разметку (JSX)
+
+const headerComponent = () => {
+	return (
+		<header>
+			<ul>
+				<li>
+					<a>Home</a>
+				</li>
+				<li>
+					<a>Main</a>
+				</li>
+				<li>
+					<a>Shop</a>
+				</li>
+				<li>
+					<a>Login</a>
+				</li>
+			</ul>
+		</header>
+	)
+}
+
+const App = () => {
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				{headerComponent()}
+				<img src={logo} className='App-logo' alt='logo' />
+				<a
+					className='App-link'
+					href='https://reactjs.org'
+					target='_blank'
+					rel='noopener noreferrer'
+				>
+					Learn React
+				</a>
+			</header>
+		</div>
+	)
+}
+
+export default App
